084bfbda91991f564f31e15d93d66ec81cf7b2ed
wr2net <wagnerr2@gmail.com>
2023-10-16 08:14:54 -0300

    feat: finalizando header e iniciando criação do sidebar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
 import {Header} from "./components/Header.jsx";
 import {Post} from "./Post.jsx";
 
-import './styles.css';
+import styles from './App.module.css';
+
+import './global.css';
+import {Sidebar} from "./components/Sidebar.jsx";
 export function App() {
   return (
     <>
       <Header />
-      <Post
-        author="Wagner Rigoli da Rosa"
-        content="asdf asdfasdf açlsdkfj açsdjf çlasdjfça jsdçfljasçldjf çlasdjçf ljasdçlfkjaçlskdjf a"
-      />
-      <Post
-        author="Maria Joaquina"
-        content="asdf asdfasdf açlsdkfj açsdjf çlasdjfça jsdçfljasçldjf çlasdjçf ljasdçlfkjaçlskdjf a"
-      />
-      <Post
-        author="Terezinha da Silva"
-        content="asdf asdfasdf açlsdkfj açsdjf çlasdjfça jsdçfljasçldjf çlasdjçf ljasdçlfkjaçlskdjf a"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Wagner Rigoli da Rosa"
+            content="asdf asdfasdf açlsdkfj açsdjf çlasdjfça jsdçfljasçldjf çlasdjçf ljasdçlfkjaçlskdjf a"
+          />
+          <Post
+            author="Maria Joaquina"
+            content="asdf asdfasdf açlsdkfj açsdjf çlasdjfça jsdçfljasçldjf çlasdjçf ljasdçlfkjaçlskdjf a"
+          />
+          <Post
+            author="Terezinha da Silva"
+            content="asdf asdfasdf açlsdkfj açsdjf çlasdjfça jsdçfljasçldjf çlasdjçf ljasdçlfkjaçlskdjf a"
+          />
+        </main>
+      </div>
     </>
   )
 }

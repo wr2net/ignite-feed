4a377f0dff40cfa2759e4cd623d9c66e1064b8ee
wr2net <wagnerr2@gmail.com>
2023-10-17 08:28:20 -0300

    feat: sessão de posts e estilização.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import {Header} from "./components/Header.jsx";
-import {Post} from "./Post.jsx";
+import {Post} from "./components/Post.jsx";
+import {Sidebar} from "./components/Sidebar.jsx";
 
 import styles from './App.module.css';
-
 import './global.css';
-import {Sidebar} from "./components/Sidebar.jsx";
+
 export function App() {
   return (
     <>

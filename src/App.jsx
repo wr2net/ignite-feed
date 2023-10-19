1a1b3c75862a9734b2c9d8ba114e66e0ead1c7f3
wr2net <wagnerr2@gmail.com>
2023-10-19 08:23:03 -0300

    feat: passando conteudo dos posts por parâmetros.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,51 @@ import {Sidebar} from "./components/Sidebar.jsx";
 import styles from './App.module.css';
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/wr2net.png",
+      name: "Wagner Rigoli da Rosa",
+      role: "Analista de Sistemas"
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2023-10-19 07:33:08'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO @Rocketseat"
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2023-10-17 07:33:08'),
+  },
+  {
+    id: 3,
+    author: {
+      avatarUrl: "https://github.com/maykbrito.png",
+      name: "Mayk Brito",
+      role: "Educator @Rocketseat"
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2023-10-18 07:33:08'),
+  }
+]
+
 export function App() {
   return (
     <>
@@ -12,18 +57,19 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Wagner Rigoli da Rosa"
-            content="asdf asdfasdf açlsdkfj açsdjf çlasdjfça jsdçfljasçldjf çlasdjçf ljasdçlfkjaçlskdjf a"
-          />
-          <Post
-            author="Maria Joaquina"
-            content="asdf asdfasdf açlsdkfj açsdjf çlasdjfça jsdçfljasçldjf çlasdjçf ljasdçlfkjaçlskdjf a"
-          />
-          <Post
-            author="Terezinha da Silva"
-            content="asdf asdfasdf açlsdkfj açsdjf çlasdjfça jsdçfljasçldjf çlasdjçf ljasdçlfkjaçlskdjf a"
-          />
+          {
+            posts.map(post => {
+              return (
+                <>
+                  <Post
+                    author={post.author}
+                    content={post.content}
+                    publishedAt={post.publishedAt}
+                  />
+                </>
+              )
+            })
+          }
         </main>
       </div>
     </>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,30 +1,61 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import styles from './Post.module.css'
 import {Comment} from "./Comment.jsx";
 import {Avatar} from "./Avatar.jsx";
 
-export function Post() {
+export function Post({ author, content, publishedAt }) {
+
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'de' yyyy 'às' HH:mm", {
+    locale: ptBR,
+  })
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  })
+
+  // eslint-disable-next-line react/prop-types
+  const publishedDateIso = publishedAt.toISOString()
+
   return (
     <>
       <article className={styles.post}>
         <header>
           <div className={styles.author}>
-            <Avatar src="https://github.com/wr2net.png" />
+            <Avatar src={author.avatarUrl} />
             <div className={styles.authorInfo}>
-              <strong>Wagner Rigoli da Rosa</strong>
-              <span>Analista de Sistemas</span>
+              <strong>{author.name}</strong>
+              <span>{author.role}</span>
             </div>
           </div>
-          <time title="17 de Outubro de 2023 às 06:13" dateTime="2023-10-17 06:45:38">Publicado há 1h</time>
+          <time title={publishedDateFormatted} dateTime={publishedDateIso}>Publicado {publishedDateRelativeToNow}</time>
+
         </header>
 
         <div className={styles.content}>
-          <p>Fala galeraa 👋</p>
-
-          <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-          <p>👉&nbsp;&nbsp;<a href=""> jane.design/doctorcare</a></p>
-          <p>
-            <a href="">#novoprojeto</a>&nbsp;<a href="">#nlw</a> <a href="">#rocketseat</a>
-          </p>
+          {
+            content.map(line => {
+              if (line.type === 'paragraph') {
+                return (
+                  <>
+                    <p>
+                      {line.content}
+                    </p>
+                  </>
+                );
+              } else if (line.type === 'link') {
+                return (
+                  <>
+                    <p>
+                      <a href='#'>{line.content}</a>
+                    </p>
+                  </>
+                );
+              }
+            })
+          }
         </div>
 
         <form className={styles.commentForm}>
